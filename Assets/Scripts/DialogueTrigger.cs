3359093dd80a14470d560fe7552642f390951171
ataylor296 <ataylor296@ufl.edu>
2019-03-11 17:29:52 -0400

    Added complete Dialogue

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+	public Dialogue dialogue;
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player" && Input.GetKeyDown("z"))
+		{
+			TriggerDialogue();
+		}
+	}
+	public void TriggerDialogue()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+	}
+}
